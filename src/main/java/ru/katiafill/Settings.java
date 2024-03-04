8689c7b341e11606c3fc418107a0a7e74f0e6f97
katiafill <katiafill@mail.ru>
2024-03-04 12:03:58 +0700

    Create settings

---
+++ src/main/java/ru/katiafill/Settings.java
@@ -0,0 +1,49 @@
+package ru.katiafill;
+
+public class Settings {
+    private int storeSize;
+    private int numberOfProducers;
+    private int numberOfConsumers;
+    private int timeoutProducer;
+    private int timeoutConsumer;
+
+    public int getStoreSize() {
+        return storeSize;
+    }
+
+    public void setStoreSize(int storeSize) {
+        this.storeSize = storeSize;
+    }
+
+    public int getNumberOfProducers() {
+        return numberOfProducers;
+    }
+
+    public void setNumberOfProducers(int numberOfProducers) {
+        this.numberOfProducers = numberOfProducers;
+    }
+
+    public int getNumberOfConsumers() {
+        return numberOfConsumers;
+    }
+
+    public void setNumberOfConsumers(int numberOfConsumers) {
+        this.numberOfConsumers = numberOfConsumers;
+    }
+
+    public int getTimeoutProducer() {
+        return timeoutProducer;
+    }
+
+    public void setTimeoutProducer(int timeoutProducer) {
+        this.timeoutProducer = timeoutProducer;
+    }
+
+    public int getTimeoutConsumer() {
+        return timeoutConsumer;
+    }
+
+    public void setTimeoutConsumer(int timeoutConsumer) {
+        this.timeoutConsumer = timeoutConsumer;
+    }
+}
---
+++ src/main/java/ru/katiafill/SettingsLoader.java
@@ -0,0 +1,45 @@
+package ru.katiafill;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class SettingsLoader {
+    private static final Logger logger = LoggerFactory.getLogger(SettingsLoader.class);
+
+    private static final String STORE_SIZE = "storeSize";
+    private static final String NUMBER_OF_PRODUCERS = "numberOfProducers";
+    private static final String NUMBER_OF_CONSUMERS = "numberOfConsumers";
+    private static final String TIMEOUT_PRODUCER = "timeoutProducer";
+    private static final String TIMEOUT_CONSUMER = "timeoutConsumer";
+
+    public static Settings load(String fileName) {
+        try {
+            Properties properties = new Properties();
+            properties.load(new FileReader(fileName));
+            return getSettings(properties);
+        } catch (IOException ex) {
+            logger.error("Exception occurred while loading " + fileName, ex);
+            return null;
+        }
+    }
+
+    private static Settings getSettings(Properties properties) {
+        Settings settings = new Settings();
+
+        settings.setStoreSize(getProperty(STORE_SIZE, properties));
+        settings.setNumberOfConsumers(getProperty(NUMBER_OF_CONSUMERS, properties));
+        settings.setNumberOfProducers(getProperty(NUMBER_OF_PRODUCERS, properties));
+        settings.setTimeoutConsumer(getProperty(TIMEOUT_CONSUMER, properties));
+        settings.setTimeoutProducer(getProperty(TIMEOUT_PRODUCER, properties));
+
+        return settings;
+    }
+
+    private static int getProperty(String key, Properties properties) {
+        return Integer.parseInt(properties.getProperty(key));
+    }
+}
