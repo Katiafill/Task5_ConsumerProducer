ecb39d6d8566951e21470b09cd590f36211aa026
katiafill <katiafill@mail.ru>
2024-03-04 12:03:42 +0700

    Create consumer

---
+++ src/main/java/ru/katiafill/Consumer.java
@@ -0,0 +1,45 @@
+package ru.katiafill;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Consumer implements Runnable {
+    private static int nextId = 0;
+    private static final Logger logger = LoggerFactory.getLogger(Consumer.class);
+
+    private final int id;
+    private final Store store;
+    private final int timeout;
+    private boolean isActive = false;
+
+    public Consumer(Store store, int timeout) {
+        this.id = ++nextId;
+        this.store = store;
+        this.timeout = timeout;
+    }
+
+    public void stop() {
+        isActive = false;
+    }
+
+    @Override
+    public void run() {
+        logger.info("Run " + this);
+        isActive = true;
+
+        while (isActive) {
+            try {
+                Thread.sleep(timeout);
+            } catch (InterruptedException ex) {
+                logger.error("Exception sleep " + this, ex);
+            }
+
+            store.get();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Consumer " + id;
+    }
+}
